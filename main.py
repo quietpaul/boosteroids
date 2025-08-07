a16268078a5cbe49b871c06910e4695c82ae9eb5
quietpaul <p.woodworth91@gmail.com>
2025-08-06 20:06:22 -0700

    Reviewing codebase against lessons

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     Clock = pygame.time.Clock()
     dt = 0
+    player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
     while pygame.get_init():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
         Clock.tick(60)
         dt = Clock.tick(60) / 1000
