bd9d042fd2367e22a48f9917a31d2c32f1c00ffb
quietpaul <p.woodworth91@gmail.com>
2025-08-06 21:09:36 -0700

    Completed player movement & Group setups

---
+++ asteroid.py
@@ -0,0 +1,8 @@
+from circleshape import *
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        self.init(x, y, radius)
+
+    def update():
+        speed += self.velocity * dt
--- main.py
+++ main.py
@@ -4,23 +4,26 @@ from player import *
 
 def main():
     pygame.init()
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     Clock = pygame.time.Clock()
     dt = 0
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
     player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
     while pygame.get_init():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updateable.update(dt)
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
-        Clock.tick(60)
         dt = Clock.tick(60) / 1000
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
