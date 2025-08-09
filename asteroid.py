8385aed35825649bd4a5ee3bb53794b4f3c56608
quietpaul <p.woodworth91@gmail.com>
2025-08-09 15:44:25 -0700

    got player moving

--- asteroid.py
+++ asteroid.py
@@ -4,5 +4,11 @@ class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         self.init(x, y, radius)
 
-    def update():
+    def update(self, speed):
         speed += self.velocity * dt
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, (255,255,255), self.position, self.radius, 2) 
+
+
+
--- main.py
+++ main.py
@@ -12,8 +12,12 @@ def main():
     dt = 0
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updateable, drawable)
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = (updateable)
     player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
+    asteroid_field = AsteroidField()
     while pygame.get_init():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
