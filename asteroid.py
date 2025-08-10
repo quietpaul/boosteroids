7793630499abc249d1d5fbbb627f1977a58a97dd
quietpaul <p.woodworth91@gmail.com>
2025-08-09 22:05:45 -0700

    asteroid pathing implemented

--- asteroid.py
+++ asteroid.py
@@ -2,13 +2,12 @@ from circleshape import *
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
-        self.init(x, y, radius)
-
-    def update(self, speed):
-        speed += self.velocity * dt
+        super().__init__(x, y, radius)
 
     def draw(self, screen):
-        pygame.draw.circle(screen, (255,255,255), self.position, self.radius, 2) 
+        pygame.draw.circle(screen, (255,255,255), (int(self.position.x), int(self.position.y)), self.radius, 2)
 
+    def update(self, dt):
+        self.position += self.velocity * dt
 
 
--- main.py
+++ main.py
@@ -1,29 +1,40 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
+
 
 def main():
+    # initialize the game & print status
     pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     Clock = pygame.time.Clock()
     dt = 0
-    updateable = pygame.sprite.Group()
+    
+    # define container groups
+    updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
-    Player.containers = (updateable, drawable)
-    Asteroid.containers = (asteroids, updateable, drawable)
-    AsteroidField.containers = (updateable)
+    
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    
     player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
     asteroid_field = AsteroidField()
+    
+    # main game loop
     while pygame.get_init():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        updateable.update(dt)
+        updatable.update(dt)
         for d in drawable:
             d.draw(screen)
         pygame.display.flip()
